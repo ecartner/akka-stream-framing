bf2a43bf950fad178dfd2f5cc0b71bc97a6d78a0
Eric Cartner <eric@westportsolutions.us>
2019-10-03 11:58:05 -0500

    Try to create a stream of frames from a file

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -1,20 +1,41 @@
 package com.example
 
+import java.nio.ByteOrder
+import java.nio.file.Paths
+
+import akka.Done
 import akka.actor.ActorSystem
 import akka.stream._
 import akka.stream.scaladsl._
-import akka.{Done, NotUsed}
 
 import scala.concurrent._
+import scala.util.{Failure, Success}
 
 object Main extends App {
   implicit val system: ActorSystem = ActorSystem("QuickStart")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val ec: ExecutionContext = system.dispatcher
 
-  val source: Source[Int, NotUsed] = Source(1 to 100)
+  val file = Paths.get("testdata", "data.bin")
+  val source = FileIO.fromPath(file)
 
-  val done: Future[Done] = source.runForeach(i => println(i))
+  def computeSize(offsetBytes: Array[Byte], u: Int): Int = {
+    val s = SimpleHeader.frameSize(u).getOrElse(0)
+    println(f"Num offset bytes: ${offsetBytes.length}, u: $u%X ($u%d), frame size: $s%d")
+    s - 4
+  }
 
-  done.onComplete(_ => system.terminate())
+  val framer = Framing.lengthField(fieldLength = 4, fieldOffset = 0, maximumFrameLength = 16,
+    byteOrder = ByteOrder.BIG_ENDIAN, computeFrameSize = computeSize)
+
+  val done: Future[Done] = source.via(framer).runForeach(bs => println(s"bytes in frame: ${bs.length}"))
+
+  done.onComplete {
+    case Failure(exception) =>
+      println(s"Failed with ${exception.getMessage}")
+      system.terminate()
+    case Success(value) =>
+      println(s"Success: $value")
+      system.terminate()
+  }
 }
